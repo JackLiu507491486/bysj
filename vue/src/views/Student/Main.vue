2414cf6d7a98c72c50640d8caccbe975d5fb9b46
JackLiu507491486 <129079498+JackLiu507491486@users.noreply.github.com>
2024-05-01 00:30:21 +0800

    接入文心一言

---
+++ vue/src/views/Student/Main.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <el-card style="height: 87vh">
+    <el-card style="height: 76vh "></el-card>
+    <el-card style="margin-top: 10px; height: 5vh">
+      <el-form-item label="学号" prop="ID">
+        <el-input v-model="question" disabled></el-input>
+      </el-form-item>
+    </el-card>
+  </el-card>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: "Main",
+  data(){
+    return{
+      question:'',
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
